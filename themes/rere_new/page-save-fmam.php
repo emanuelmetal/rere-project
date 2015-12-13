6745e135d1d3629069c824d28e68ad170c95465a
emanuel <emanuelmetal@gmail.com>
2015-12-13 18:59:08 -0300

    FMAM email for buyer or seller.

--- themes/rere_new/page-save-fmam.php
+++ themes/rere_new/page-save-fmam.php
@@ -11,8 +11,17 @@ global $wpdb;
 
 $content = addslashes($_POST["content"]);
 $form = addslashes($_POST["form"]);
+$message = strip_tags($_POST["message"]);
 $query = "INSERT INTO wp_fmam_json (content, form) VALUES ('{$content}', '{$form}')";
 
 $results = $wpdb->query($query);
+
+if ($form == 'buyers') {
+    $subject = 'Find me a Match request from a buyer';
+}
+else {
+    $subject = 'Find me a Match request from a seller';
+}
+wp_mail('ljgrere@gmail.com', $subject, $message);
 
 echo "1";
